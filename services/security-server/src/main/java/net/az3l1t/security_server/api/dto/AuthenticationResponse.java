5c1f11040a8fe3d6ac2c3d9e619317c54278fbf4
az3l1t <eternity_cr9p@mail.ru>
2024-07-24 19:31:10 +0300

    made the auth-server

---
+++ services/security-server/src/main/java/net/az3l1t/security_server/api/dto/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package net.az3l1t.security_server.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String token;
+}
